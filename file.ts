27e8562ac91a4c277ade1baba94a23a520f0894d
tashanduncan1994@gmail.com <tashanduncan1994@gmail.com>
2023-02-11 08:25:56 +0000

    code cleanup

--- file.ts
+++ file.ts
@@ -1,29 +1,26 @@
-
-export function isUnique(array: any[], index: number, amount:number) {
-  const uniqueCharacters = new Set<string>()
+export function isUnique(array: any[], index: number, amount: number) {
+  const uniqueCharacters = new Set<string>();
 
   for (let i = 0; i < amount; i++) {
-   uniqueCharacters.add(array[index + i])
-   console.log(uniqueCharacters)
+    uniqueCharacters.add(array[index + i]);
   }
 
-  if(uniqueCharacters.size === 4){
-    return true
+  if (uniqueCharacters.size === amount) {
+    return true;
   } else {
-    return false
+    return false;
   }
 }
 
 export function tuningTrouble(data: string, amount: number) {
-  const dataArray = data.split('')
-  let result = 0
+  const dataArray = data.split("");
+  let result = 0;
 
   for (let i = 0; i < dataArray.length; i++) {
-    if(isUnique(dataArray, i, amount)){
-      result = i + amount
+    if (isUnique(dataArray, i, amount)) {
+      result = i + amount;
       break;
     }
-    
   }
-  return result
+  return result;
 }
