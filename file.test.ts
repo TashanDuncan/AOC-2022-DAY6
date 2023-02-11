c7da3426f4668d54d38d28bfa1ed2df0d323c9a4
tashanduncan1994@gmail.com <tashanduncan1994@gmail.com>
2023-02-11 08:17:46 +0000

    add part 1 tests

--- file.test.ts
+++ file.test.ts
@@ -1,7 +1,27 @@
-import { test } from "./file";
-describe('', () => { 
-  it('should ', () => {
-    
+import { isUnique, tuningTrouble } from "./file";
+describe('Tuning Trouble', () => { 
+  it('should return true if the next 3 elements of an array are unique', () => {
+      
+      expect(isUnique([1,2,3,4,5], 0, 4)).toEqual(true)
+      expect(isUnique([1,2,2,4,5], 0, 4)).toEqual(false)
   });
+
+  it('should work with examples from readme', () => {
+    
+    expect(tuningTrouble('bvwbjplbgvbhsrlpgdmjqwftvncz', 4)).toEqual(5)
+    expect(tuningTrouble('nppdvjthqldpwncqszvftbrmjlhg', 4)).toEqual(6)
+    expect(tuningTrouble('nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg', 4)).toEqual(10)
+    expect(tuningTrouble('zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw', 4)).toEqual(11)
+  });
+
+  it('should work with examples from readme part 2', () => {
+    
+    expect(tuningTrouble('bvwbjplbgvbhsrlpgdmjqwftvncz', 4)).toEqual(5)
+    expect(tuningTrouble('nppdvjthqldpwncqszvftbrmjlhg', 4)).toEqual(6)
+    expect(tuningTrouble('nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg', 4)).toEqual(10)
+    expect(tuningTrouble('zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw', 4)).toEqual(11)
+  });
+
+
 
  })
