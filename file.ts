115dc20f540dc22455ea65535a745929c7e84554
tashanduncan1994@gmail.com <tashanduncan1994@gmail.com>
2023-02-11 08:18:03 +0000

    add logic

--- file.ts
+++ file.ts
@@ -1,4 +1,29 @@
 
-export function test() {
-  
-} 
+export function isUnique(array: any[], index: number, amount:number) {
+  const uniqueCharacters = new Set<string>()
+
+  for (let i = 0; i < amount; i++) {
+   uniqueCharacters.add(array[index + i])
+   console.log(uniqueCharacters)
+  }
+
+  if(uniqueCharacters.size === 4){
+    return true
+  } else {
+    return false
+  }
+}
+
+export function tuningTrouble(data: string, amount: number) {
+  const dataArray = data.split('')
+  let result = 0
+
+  for (let i = 0; i < dataArray.length; i++) {
+    if(isUnique(dataArray, i, amount)){
+      result = i + amount
+      break;
+    }
+    
+  }
+  return result
+}
