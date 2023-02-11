8b5922f3b0fe61f5aeeab423ced1f59c935f085b
tashanduncan1994@gmail.com <tashanduncan1994@gmail.com>
2023-02-11 08:26:43 +0000

    clean up tests

--- file.test.ts
+++ file.test.ts
@@ -1,27 +1,21 @@
 import { isUnique, tuningTrouble } from "./file";
-describe('Tuning Trouble', () => { 
-  it('should return true if the next 3 elements of an array are unique', () => {
-      
-      expect(isUnique([1,2,3,4,5], 0, 4)).toEqual(true)
-      expect(isUnique([1,2,2,4,5], 0, 4)).toEqual(false)
+describe("Tuning Trouble", () => {
+  it("should return true if the next 3 elements of an array are unique", () => {
+    expect(isUnique([1, 2, 3, 4, 5], 0, 4)).toEqual(true);
+    expect(isUnique([1, 2, 2, 4, 5], 0, 4)).toEqual(false);
   });
 
-  it('should work with examples from readme', () => {
-    
-    expect(tuningTrouble('bvwbjplbgvbhsrlpgdmjqwftvncz', 4)).toEqual(5)
-    expect(tuningTrouble('nppdvjthqldpwncqszvftbrmjlhg', 4)).toEqual(6)
-    expect(tuningTrouble('nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg', 4)).toEqual(10)
-    expect(tuningTrouble('zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw', 4)).toEqual(11)
+  it("should work with examples from readme", () => {
+    expect(tuningTrouble("bvwbjplbgvbhsrlpgdmjqwftvncz", 4)).toEqual(5);
+    expect(tuningTrouble("nppdvjthqldpwncqszvftbrmjlhg", 4)).toEqual(6);
+    expect(tuningTrouble("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 4)).toEqual(10);
+    expect(tuningTrouble("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 4)).toEqual(11);
   });
 
-  it('should work with examples from readme part 2', () => {
-    
-    expect(tuningTrouble('bvwbjplbgvbhsrlpgdmjqwftvncz', 14)).toEqual(23)
-    expect(tuningTrouble('nppdvjthqldpwncqszvftbrmjlhg', 14)).toEqual(23)
-    expect(tuningTrouble('nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg', 14)).toEqual(29)
-    expect(tuningTrouble('zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw', 14)).toEqual(26)
+  it("should work with examples from readme part 2", () => {
+    expect(tuningTrouble("bvwbjplbgvbhsrlpgdmjqwftvncz", 14)).toEqual(23);
+    expect(tuningTrouble("nppdvjthqldpwncqszvftbrmjlhg", 14)).toEqual(23);
+    expect(tuningTrouble("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 14)).toEqual(29);
+    expect(tuningTrouble("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 14)).toEqual(26);
   });
-
-
-
- })
+});
